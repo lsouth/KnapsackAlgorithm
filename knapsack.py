b2a8ee8b687aad1eae62943764ab21831023384d
lsouth <lsouth269@gmail.com>
2017-09-25 19:40:40 -0600

    Change master knapsack implementation

--- knapsack.py
+++ knapsack.py
@@ -1,47 +1,64 @@
 import sys
+import numpy as np
 import time
 
+class Knapsack:
+    W = 0
+    N = 0
+    items = []
+    table = [[]]
 
-def knapsackTable(items, maxWeight):
-    finalVals = [[0] * (maxWeight + 1)
-                 for i in range(len(items) + 1)]
+    def __str__(self):
+        return np.matrix(self.table)
 
+    def read_input(self, filename):
+        #expected input:
+        # W (total capacity)
+        # v1 w1
+        # v2 w2
+        # ...
+        # vn wn
+        # f = open(filename)
+        with open(filename) as f:
+            self.W = int(f.readline())
+            self.N = int(f.readline())
+            self.items.append([0,0])
+            for i in range(self.N):
+                self.items.append(f.readline().split())
+        self.table = [[0 for x in range(self.W + 1)] for y in range(self.N + 1)]
+
+    def make_table(self):
+        for i in range(0,self.W):
+            self.table[0][i] = 0
+        for i in range(1,self.N + 1):
+            for w in range(self.W + 1):
+                weight_i = int(self.items[i][1])
+                value_i = int(self.items[i][0])
+            #    print("weight_i = ", weight_i, ", w = ", w)
+                if weight_i > w:
+                    self.table[i][w] = self.table[i - 1][w]
+                else:
+                    self.table[i][w] = max(self.table[i-1][w], value_i + self.table[i-1][w-weight_i])
+        return self.table[self.N - 1][self.W - 1]
+
+    def backtrack(self):
+        opt = self.table[self.N][self.W]
+        current_weight = self.W
+        for i in range(self.N, 0, -1):
+            if current_weight == 0:
+                return
+            check = self.table[i - 1][self.W - int(self.items[i][1])]
+            if check + int(self.items[i][0]) == opt:
+                self.W -= int(self.items[i][1])
+                opt = self.table[i][self.W]
+
+if __name__ == "__main__":
+    k = Knapsack()
+    k.read_input(sys.argv[1])
     t0 = time.time()
-
-    for i, (value, weight) in enumerate(items):
-        # skip the first line because there's no items with 0
-        i += 1
-        for capacity in range(maxWeight + 1):
-            # case 1: we can't add it
-            if weight > capacity:
-                finalVals[i][capacity] = finalVals[i - 1][capacity]
-            # case 2: either choose previous capacity, or currItem + val of previous set
-            # (but only if it fits, previous capacity - item weight)
-            else:
-                firstVal = finalVals[i - 1][capacity]
-                secondVal = finalVals[i - 1][capacity - weight] + value
-                finalVals[i][capacity] = max(firstVal, secondVal)
-
+    k.make_table()
     t1 = time.time()
-
-    # if the value was the same as the one in the previous row, no new item was chosen, do nothing
-    # then we move up to the previous row, otherwise, the item is in the knapsack and we
-    # subtract its weight from the remaining capacity
-    includedItems = []
-    i = len(items)
-    j = maxWeight
-    while i > 0:
-        if finalVals[i][j] != finalVals[i - 1][j]:
-            includedItems.append(items[i - 1])
-            j -= items[i - 1][1]
-        i -= 1
-
+    k.backtrack()
     t2 = time.time()
-
-    s1 = ('Create Matrix Time: ' + repr(t1 - t0))
-    s2 = ('Backtrack time: ' + repr(t2 - t1))
-    print(s1)
-    print(s2)
-
-    # Return the best value, and the reconstruction list
-    return finalVals[len(items)][maxWeight], includedItems
+    print("Time to build table: ", t1 - t0)
+    print("Time to backtrack: ", t2 - t1)
