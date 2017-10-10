2f0f9918c579232576417ea413eae436f169511b
Kira Miller <31229189+kiram15@users.noreply.github.com>
2017-10-10 10:59:07 -0600

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,13 +1,31 @@
 from random import randint
 import json
+import pprint
 
-def write_obj_to_json_file(filename, obj):
+def to_pretty_json(obj):
+    ret = pprint.pformat(obj, indent=1, width=100, compact=True).replace('[', '').replace(']', '\n').replace('\'', '"')
+    try:
+        json.loads(ret)
+    except ValueError:
+        print("Tried to encode as: " + ret)
+        raise
+    return ret
+
+def write_string_to_file(filename, s):
+    with open(filename, mode='w') as out:
+        out.write(s)
+        if s[-1] != '\n':
+            out.write('\n')
+
+def write_obj_to_json_file(filename, obj, prettyPrint):
     name = filename if filename.endswith('.json') else filename + '.json'
     with open(name, mode='w') as f:
-        json.dump(obj, f, separators=(' ', ':'))
+        ret = pprint.pformat(obj, indent=1, width=100, compact=True).replace('[', '').replace(']', '\n').replace(
+             ',', '')
+        json.dump(ret, f)
     return name
 
-def create_problem_set(numPairs, min, max):
+def create_problem_set(maxWeight, numPairs, min, max):
     outside_list = []
     for i in range(0, numPairs):
         inside_list = []
@@ -16,9 +34,13 @@ def create_problem_set(numPairs, min, max):
         outside_list.append(inside_list)
     total = len(outside_list)
     outside_list.insert(0, total)
+    outside_list.insert(1, '\n')
+    outside_list.insert(0, maxWeight)
+    outside_list.insert(1, '\n')
+    return outside_list
 
 if __name__ == "__main__":
     # you can access any inside_list from the outside_list and append
-    create_problem_set(50, 0, 500)
-    write_obj_to_json_file("outputFile", outside_list)
+    outside_list = create_problem_set(1000, 10, 10, 500)
+    write_obj_to_json_file("outputFile", outside_list, prettyPrint="true")
 
